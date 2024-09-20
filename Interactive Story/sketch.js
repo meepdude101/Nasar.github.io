cd3b7c932a8fda5da0783513a34ca32691294c45
Khan3903521 <3903521@spsd.sk.ca>
2024-09-20 12:04:27 -0600

    Made Moon Landscape

---
+++ Interactive Story/sketch.js
@@ -0,0 +1,29 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // Set the initial background color
+  background(0); // Space
+
+  noStroke();
+
+  fill(137, 137, 137); // Grey
+  ellipse(500, 650, 700, 300); // Moon ground
+
+  fill(94, 94, 94); // Grey
+  ellipse(100, 700, 700, 250); // Moon ground
+
+  fill(167, 168, 151); // Grey
+  ellipse(900, 800, 2000, 300); // Moon ground
+
+  fill(239, 255, 0);
+  circle(290, 100, 100);
+
+
+}
