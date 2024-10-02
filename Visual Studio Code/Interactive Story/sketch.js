e9bf297126015a277df4385792eed8f8eb34e271
Khan3903521 <3903521@spsd.sk.ca>
2024-10-02 12:16:12 -0600

    updated
    made background and astro follow mouse

--- Visual Studio Code/Interactive Story/sketch.js
+++ Visual Studio Code/Interactive Story/sketch.js
@@ -5,12 +5,96 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let currentBack = 0;
+let sunColor = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0); // Space background
   noStroke();
+  let currentBack = 0;
+ 
+}
 
+
+
+function drawAstronaut() {
+  rectMode(CENTER);
+  
+  // Helmet
+  fill(255); // White
+  rect(mouseX, mouseY + 2, 24, 24); 
+
+  fill(0); // Black 
+  rect(mouseX, mouseY, 18, 16);
+
+  fill(173, 216, 230); // Light blue 
+  rect(mouseX + 6, mouseY - 3, 5, 3);
+  
+  // Torso 
+  fill(255); // White
+  rect(mouseX, mouseY + 30, 22, 35);
+  
+  // Suit details
+  fill(255, 0, 0); // Red
+  rect(mouseX - 4, mouseY + 23, 5, 5);
+  
+  fill(0, 0, 255); // Blue
+  rect(mouseX + 4, mouseY + 23, 5, 5);
+  
+  fill(255, 255, 0); // Yellow
+  rect(mouseX, mouseY + 28, 5, 5);
+  
+  // Arms 
+  fill(255); // White
+  rect(mouseX - 15, mouseY + 25, 10, 25); // Left arm
+  rect(mouseX + 15, mouseY + 25, 10, 25); // Right arm
+  
+  // Hands 
+  fill(169, 169, 169); // Light grey
+  rect(mouseX - 15, mouseY + 40, 10, 10); // Left glove 
+  rect(mouseX + 15, mouseY + 40, 10, 10); // Right glove 
+
+  // Legs 
+  fill(255); // White
+  rect(mouseX - 6, mouseY + 60, 10, 25); // Left leg
+  rect(mouseX + 6, mouseY + 60, 10, 25); // Right leg
+
+  // Boots 
+  fill(169, 169, 169); // Grey boots
+  rect(mouseX - 6, mouseY + 75, 10, 8); // Left boot
+  rect(mouseX + 6, mouseY + 75, 10, 8); // Right boot
+}
+
+function changeBackground() {
+  // Change the background color based on currentBack value
+  if (currentBack == 0) {
+    background(0); // Black (space)
+  } else if (currentBack == 1) {
+    background(100, 149, 237); // blue
+  } else if (currentBack == 2) {
+    background(34, 139, 34); // green
+  } else if (currentBack == 3) {
+    background(255, 182, 193); // pink
+  }
+}
+
+function keyPressed(){
+  if(keyCode === ' '){}
+}
+
+function mousePressed() {
+  // Check if the middle mouse button is pressed
+  if (mouseButton === CENTER) {
+    currentBack = (currentBack + 1) % 4; // Cycle through 0, 1, 2, 3
+    changeBackground(); // Update the background based on the new state
+  }
+}
+
+
+function draw(){
+  changeBackground();
   // Landscape 
   fill(137, 137, 137); // Grey
   ellipse(500, 650, 700, 300); 
@@ -26,55 +110,5 @@ function setup() {
   circle(290, 100, 100); 
 
   
-  drawAstronaut(300, 400);
-
-}
-
-
-function drawAstronaut(x, y) {
-  rectMode(CENTER);
-  
-  // Helmet
-  fill(255); // White
-  rect(x, y + 2, 24, 24); 
-
-  fill(0); // Black 
-  rect(x, y, 18, 16);
-
-  fill(173, 216, 230); // Light blue 
-  rect(x + 6, y - 3, 5, 3);
-  
-  // Torso 
-  fill(255); // White
-  rect(x, y + 30, 22, 35);
-  
-  // Suit details
-  fill(255, 0, 0); // Red
-  rect(x - 4, y + 23, 5, 5);
-  
-  fill(0, 0, 255); // Blue
-  rect(x + 4, y + 23, 5, 5);
-  
-  fill(255, 255, 0); // Yellow
-  rect(x, y + 28, 5, 5);
-  
-  // Arms 
-  fill(255); // White
-  rect(x - 15, y + 25, 10, 25); // Left arm
-  rect(x + 15, y + 25, 10, 25); // Right arm
-  
-  // Hands 
-  fill(169, 169, 169); // Light grey
-  rect(x - 15, y + 40, 10, 10); // Left glove 
-  rect(x + 15, y + 40, 10, 10); // Right glove 
-
-  // Legs 
-  fill(255); // White
-  rect(x - 6, y + 60, 10, 25); // Left leg
-  rect(x + 6, y + 60, 10, 25); // Right leg
-
-  // Boots 
-  fill(169, 169, 169); // Grey boots
-  rect(x - 6, y + 75, 10, 8); // Left boot
-  rect(x + 6, y + 75, 10, 8); // Right boot
+  drawAstronaut();
 }
